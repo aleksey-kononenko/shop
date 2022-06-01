80664ee2992ebef6259cbd5f7112a92fe9b20907
aleksey-kononenko <kononenko104@gmail.com>
2022-06-01 18:26:38 +0300

    first commit

--- bestShop/settings_prod.py
+++ bestShop/settings_prod.py
@@ -1,12 +1,12 @@
 DEBUG = False
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '137.184.2.173']
 
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "db_shop",
-        'USER': "alex",
+        'NAME': "eShop",
+        'USER': "eShop",
         'PASSWORD': "marik2010",
         'HOST': "localhost",
         'PORT': "",  # 5432 by default
